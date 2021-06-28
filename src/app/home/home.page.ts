0a3fa0c8d8675bcb250c9665f9731b38eeeb3c44
Joao0902-source <jpg090202@gmail.com>
2021-06-28 14:10:52 -0300

    Adicionando app calculadora

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { isNumber } from 'util';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  value = '0';
+  oldValue = '0';
+
+  lastOperator = 'x';
+  readyForNewInput = true;
+  numberGroups = [
+    [7, 8, 9, 'x'],
+    [4, 5, 6, '-'],
+    [1, 2, 3, '+'],
+    [0, 'c', '/', '=']
+  ];
+
+  onButtonPress(symbol) {
+    console.log(symbol);
+
+    if (isNumber(symbol)) {
+      console.log('É um número!');
+      if (this.readyForNewInput)
+        this.value = '' + symbol;
+      else
+        this.value += '' + symbol;
+      this.readyForNewInput = false;
+    }
+    else if (symbol === 'c') {
+      this.value = '0';
+      this.readyForNewInput = true;
+    }
+    else if (symbol === '=') {
+      if (this.lastOperator === 'x')
+        this.value = '' + (parseInt(this.oldValue) * parseInt(this.value));
+      else if (this.lastOperator === '-')
+        this.value = '' + (parseInt(this.oldValue) - parseInt(this.value));
+      else if (this.lastOperator === '+')
+        this.value = '' + (parseInt(this.oldValue) + parseInt(this.value));
+      else if (this.lastOperator === '/')
+        this.value = '' + (parseInt(this.oldValue) / parseInt(this.value));
+      this.readyForNewInput = true;
+    }
+    else { // operator
+      this.readyForNewInput = true;
+      this.oldValue = this.value;
+      this.lastOperator = symbol;
+    }
+  }
+}
